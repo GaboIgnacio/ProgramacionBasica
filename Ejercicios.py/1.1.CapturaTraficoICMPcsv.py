03bd6e73af3158c458236b86e502061182674db9
GaboIgnacio <gabriel24ignacio@gmail.com>
2025-05-30 20:00:53 -0400

    nuevos ejercicios

--- Ejercicios.py/1.1.CapturaTraficoICMPcsv.py
+++ Ejercicios.py/1.1.CapturaTraficoICMPcsv.py
@@ -50,32 +50,3 @@ print(f"\nLos datos han sido guardados en '{ARCHIVO_CSV}'.")
 #         except AttributeError:
 #             continue
 # print(f"\nCaptura finalizada. Paquetes ICMP guardados en '{archivo_csv}' ")
-
-import pyshark
-import csv
-INTERFAZ = 'Wi-Fi'
-ARCHIVO_CSV = 'captura_icmp.csv'
-paquetes_icmp = []
-print("Iniciando captura de paquetes ICMP...\n")
-captura = pyshark.LiveCapture(interface=INTERFAZ, display_filter='icmp')
-for packet in captura.sniff_continuously(packet_count=10):
-    try:
-        hora = packet.sniff_time
-        origen = packet.ip.src
-        destino = packet.ip.dst
-        tipo = packet.icmp.type
-        codigo = packet.icmp.code
-        paquetes_icmp.append([hora, origen, destino, tipo, codigo])
-        print(f"Hora: {hora}, Origen: {origen}, Destino: {destino}, Tipo ICMP: {tipo}, Código: {codigo}")
-    except AttributeError:
-        continue  
-print("\nLista de paquetes ICMP capturados:")
-for paquete in paquetes_icmp:
-    print(paquete)
-with open(ARCHIVO_CSV, mode='w', newline='') as archivo:
-    writer = csv.writer(archivo)
-    writer.writerow(['Hora', 'IP Origen', 'IP Destino', 'Tipo ICMP', 'Código ICMP'])
-    writer.writerows(paquetes_icmp)
-
-print(f"\nLos datos han sido guardados en '{ARCHIVO_CSV}'.")
-
